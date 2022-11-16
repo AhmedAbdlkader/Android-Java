a79af83c82b448213ce56343db5f2786c6721172
= <=>
2022-11-16 16:41:00 +0100

    commitaddProd

--- app/src/main/java/com/esprit/bluegymproject/database/AppDataBase.java
+++ app/src/main/java/com/esprit/bluegymproject/database/AppDataBase.java
@@ -7,20 +7,23 @@ import androidx.room.Room;
 import androidx.room.RoomDatabase;
 
 import com.esprit.bluegymproject.dao.CoachDao;
+import com.esprit.bluegymproject.dao.ProduitDao;
 import com.esprit.bluegymproject.dao.TypeAbnDao;
 import com.esprit.bluegymproject.dao.UserDao;
 import com.esprit.bluegymproject.entity.Coach;
+import com.esprit.bluegymproject.entity.Produits;
 import com.esprit.bluegymproject.entity.TypeAbn;
 import com.esprit.bluegymproject.entity.User;
 
 
-@Database(entities = {User.class ,Coach.class , TypeAbn.class}, version = 3, exportSchema = false)
+@Database(entities = {User.class ,Coach.class , TypeAbn.class, Produits.class}, version = 3, exportSchema = false)
 public abstract class AppDataBase extends RoomDatabase {
 
     private static AppDataBase instance;
 
 
     public abstract UserDao userDao();
+    public abstract ProduitDao produitDao();
     public abstract CoachDao coachDao();
     public abstract TypeAbnDao typeAbnDao();
 
